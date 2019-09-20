76e61fa4cc0e6bb421109f077321592d7a4f43e1
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-09-19 23:11:00 -0500

    set the axes to have equal scale

--- stl_viewer.py
+++ stl_viewer.py
@@ -9,6 +9,35 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from stl import Mesh
 
 
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+      ax: a matplotlib axis, e.g., as output from plt.gca().
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5 * max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
+
+
 def get_limit(mesh, column, scale=.1):
     min_val = np.hstack((mesh.v0[:, column].min(), mesh.v1[:, column].min(), mesh.v2[:, column].min())).min()
     max_val = np.hstack((mesh.v0[:, column].max(), mesh.v1[:, column].max(), mesh.v2[:, column].max())).max()
@@ -59,10 +88,9 @@ x_lim, y_lim, z_lim = get_limit(your_mesh, 0), get_limit(your_mesh, 1), get_limi
 axes.set_xlim(*x_lim)
 axes.set_ylim(*y_lim)
 axes.set_zlim(*z_lim)
+set_axes_equal(axes)
 
 out = np.array(list(zip(your_mesh.v0[:, :2], your_mesh.v1[:, :2], your_mesh.v2[:, :2])))
-
-a = out.min()
 
 fig: Figure = plt.figure()
 axes: Axes = fig.gca()
@@ -74,6 +102,7 @@ axes.add_patch(get_2d_box(your_mesh))
 
 axes.set_xlim(*x_lim[::-1])
 axes.set_ylim(*y_lim[::-1])
+axes.set_aspect('equal')
 
 # Show the plot to the screen
 plt.show()
